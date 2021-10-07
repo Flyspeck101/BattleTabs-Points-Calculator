1d98a16b1305446adc58e1e94619bfd3c93ea709
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-10-08 10:29:55 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let wins = 0;
 let losses = 0;
 
 let onChange = () => {
-  
+  const points = losses + 5 * wins;
+  const winRate = 100 * wins / (wins + losses);
+  const experience = wins * 1.5 + losses; 
+  const skill = winRate * experience / 100;
+  document.getElementById("points").innerText = points;
+  document.getElementById("win-rate").innerText = winRate;
+  document.getElementById("points").innerText = experience;
+  document.getElementById("points").innerText = skill;
 };
 
 document.getElementById("wins").addEventListener("change", e => {
