bbb4788bd3518288c19917cddd7024facfe8f29c
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2021-10-08 10:19:07 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+let wins = 0;
+let losses = 0;
+
 let onChange = e => {
   
-}
+};
 
-document.getElementById("wins").addEventListener("change", onChange);
+document.getElementById("wins").addEventListener("change", e => {
+  if (typeof JSON.parse(e.target.value) == "number") {
+    wins = e.target.value;
+    else
+  onChange);
 document.getElementById("losses").addEventListener("change", onChange);
